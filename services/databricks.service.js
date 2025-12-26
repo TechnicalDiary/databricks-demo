3c3bf5af2350c6e1b37cae3215412b8f635a1e85
shahbaz sharif <SHAHBAZ@Mac.lan>
2025-12-26 20:16:36 +0530

    token issue fixed

--- services/databricks.service.js
+++ services/databricks.service.js
@@ -1,53 +1,13 @@
 const { DBSQLClient } = require('@databricks/sql');
-const axios = require('axios');
 
-async function getAccessToken() {
-    try {
-        const host = process.env.DATABRICKS_HOST;
-        const clientId = process.env.DATABRICKS_CLIENT_ID;
-        const clientSecret = process.env.DATABRICKS_CLIENT_SECRET;
-        if (!host) {
-            console.error("Host not found");
-            throw new Error("Host not found");
-        } else if (!clientId) {
-            console.error("ClientId not found");
-            throw new Error("ClientId not found");
-        } else if (!clientSecret) {
-            console.error("Client secredt not found");
-            throw new Error("Client secredt not found");
-        }
-        console.log('ENV:', process.env)
-        // Databricks OAuth token endpoint call karein
-        const response = await axios.post(
-            `https://${host}/oidc/token`,
-            new URLSearchParams({
-                'grant_type': 'client_credentials',
-                'scope': 'all-apis'
-            }),
-            {
-                auth: {
-                    username: clientId,
-                    password: clientSecret
-                }
-            }
-        );
-
-        return response.data.access_token;
-    } catch (error) {
-        console.error("Token Fetch Error:", error.response?.data || error.message);
-        throw new Error("Could not fetch OAuth token");
-    }
-}
-
-async function executeQuery(query) {
+// Token ab hum function argument mein pass karenge
+async function executeQuery(query, userToken) {
     const client = new DBSQLClient();
     try {
-        const token = await getAccessToken();
-
         await client.connect({
             host: process.env.DATABRICKS_HOST,
             path: `/sql/1.0/warehouses/${process.env.DATABRICKS_WAREHOUSE_ID}`,
-            token: token
+            token: userToken // User ka token jo header se aaya
         });
 
         const session = await client.openSession();
@@ -58,7 +18,7 @@ async function executeQuery(query) {
         await session.close();
         return result;
     } catch (error) {
-        console.error("Query Execution Error:", error);
+        console.error("Query Error:", error);
         throw error;
     } finally {
         await client.close();
